e4f47acf1f28d85f15170ba9fcc08c573369ae8b
Robert Nester Odhiambo <robertnesterodhiambo@gmail.com>
2024-01-13 21:59:54 +0300

    first zip code

---
+++ zip functions/zipfunctions.py
@@ -0,0 +1,7 @@
+# ziping on;y wrks in pyhton 2 make sure hte list are of the same size
+l1 = ["1","2","3","4","5"]
+l2 = ["6","7","8","9","10"]
+
+# in python 3 or greater it worsk like this
+zipped = list(zip(l1, l2))
+print(zipped)
