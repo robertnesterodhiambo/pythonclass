3282944c756a78c8dd316113278c51e213e3c5e6
Robert Nester Odhiambo <robertnesterodhiambo@gmail.com>
2024-01-13 22:02:31 +0300

    unzipping

--- zip functions/zipfunctions.py
+++ zip functions/zipfunctions.py
@@ -5,3 +5,8 @@ l2 = ["6","7","8","9","10"]
 # in python 3 or greater it worsk like this
 zipped = list(zip(l1, l2))
 print(zipped)
+
+#unzipping  in python 3 we have to make sure its a list before unziping 
+
+unzipped = list( zip(*zipped))
+print(unzipped)
