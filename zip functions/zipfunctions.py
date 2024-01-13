8a2df97a963a9ac16f84780185c7a05aaa394c10
Robert Nester Odhiambo <robertnesterodhiambo@gmail.com>
2024-01-13 22:20:59 +0300

    zipping final

--- zip functions/zipfunctions.py
+++ zip functions/zipfunctions.py
@@ -10,3 +10,27 @@ print(zipped)
 
 unzipped = list( zip(*zipped))
 print(unzipped)
+
+# zipped are used to read variable that are outside he for loops and other funtiosn into them 
+#especially when getting a bunch of things into scope
+#### exampes
+
+for i in range(5):
+    l1[i]
+    l2[[i]]
+    print(l1)
+
+for (list1, list2) in zip(l1,l2):
+    print(l1)
+    print(l2)
+
+items = ["apple","banana","orange"]
+counts = [3, 15, 56]
+prices = [12, 32, 36]
+
+sentences = [] 
+for (item, count, price) in (items, counts, prices):
+    item, count, price = str(item), str(count), str(price)
+    sentence = "I bought " + count + " " + item + "s at " + price + "shillings each ."
+    sentences.append(sentence)
+    print(sentences)
