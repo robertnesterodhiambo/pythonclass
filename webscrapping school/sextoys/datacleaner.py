ac1cdfd356303528ce5e0c2de0f53f16847b1309
Robert Nester Odhiambo <odhiamborobert447@gmail.com>
2024-08-27 00:00:48 +0300

    file extraction and scraping coplete

--- webscrapping school/sextoys/datacleaner.py
+++ webscrapping school/sextoys/datacleaner.py
@@ -11,7 +11,7 @@ df_cleaned = df.dropna()
 df_cleaned['EABN'] = df_cleaned['product_link'].str.extract(r'(\d+)\.html')
 
 # Select the first 10,000 rows
-df_first_10000 = df_cleaned.iloc[:10000]
+df_first_10000 = df_cleaned #.iloc[:10000]
 
 # Save the first 10,000 rows to an Excel file
 output_file_path = 'cleaned_product_titles_and_links_first_10000_with_EABN.xlsx'
