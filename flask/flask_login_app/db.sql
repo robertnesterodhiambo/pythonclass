954030d38f81086e835b7d1e8a6b1215b57d8ad4
Robert Nester Odhiambo <odhiamborobert447@gmail.com>
2024-09-25 12:51:23 +0300

    DB

--- flask/flask_login_app/db.sql
+++ flask/flask_login_app/db.sql
@@ -1,12 +1,12 @@
-CREATE DATABASE flask_login;
+    CREATE DATABASE flask_login;
 
-USE flask_login;
+    USE flask_login;
 
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    email VARCHAR(100) UNIQUE,
-    password VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+    CREATE TABLE users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        first_name VARCHAR(50),
+        last_name VARCHAR(50),
+        email VARCHAR(100) UNIQUE,
+        password VARCHAR(255),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
