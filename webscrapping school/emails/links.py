30cc803c87acbf80cef7af002bec56ef7ed4b682
Robert Nester Odhiambo <odhiamborobert447@gmail.com>
2024-10-02 11:04:39 +0300

    importation of files

---
+++ webscrapping school/emails/links.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Load the CSV file into a DataFrame
+df = pd.read_csv('collected_links.csv')
+
+# Display the first few rows of the DataFrame to verify the contents
+print(df.head())
