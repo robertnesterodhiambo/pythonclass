cf17319b2eb1ba67758e0b62a7bb76e35d6469b6
Robert Nester Odhiambo <odhiamborobert447@gmail.com>
2025-02-25 10:43:17 +0300

    importing to cpanel

--- economics/flask_economics/app.py
+++ economics/flask_economics/app.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 
 # Database Configuration
 DB_CONFIG = {
-    "host": "localhost",
-    "database": "Stocks",
-    "user": "root",
-    "password": "1234",
+    "host": "104.238.220.190",
+    "database": "stocksjbetadev_Stocks",
+    "user": "stocksjbetadev",
+    "password": "qZh]R0+inyo+",
     "port": 3306,
     "charset": "utf8mb4",
 }
