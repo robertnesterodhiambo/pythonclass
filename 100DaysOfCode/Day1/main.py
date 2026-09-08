651ff0968ff7622ed76b0af0966dba0fbd37e73e
Robert Nester Odhiambo <odhiamborobert447@gmail.com>
2026-09-08 11:47:17 +0300

    String manipulations

--- 100DaysOfCode/Day1/main.py
+++ 100DaysOfCode/Day1/main.py
@@ -5,3 +5,16 @@ print("hello world")
 print("Hello Sucker  \n THis ia a string manipulation example")
 
 print("Robert " + "Odhiambo")
+
+#1. Missing double quotes before the word Day.
+print("Day 1 - String Manipulation")
+
+#2. Outer double quotes changed to single quotes.
+print('String Concatenation is done with the "+" sign.')
+
+#3. Extra indentation removed
+print('e.g. print("Hello " + "world")')
+
+#4. Extra ( in print function removed.
+print("New lines can be created with a backslash and n.")
+
