a20ed4eedf0a5b8d57a42d6b7ac5d6fa57d829d2
Robert Nester Odhiambo <odhiamborobert447@gmail.com>
2026-09-08 12:04:10 +0300

    lenth of strings and characters

--- 100DaysOfCode/Day1/input_function.py
+++ 100DaysOfCode/Day1/input_function.py
@@ -1,3 +1,5 @@
 #Allows you to enter data from terminals
 # input("What is your name")
-print("Hello " + input("What is your name?"))
+# print("Hello " + input("What is your name?"))
+
+print(len(input("What is your name? ")))
