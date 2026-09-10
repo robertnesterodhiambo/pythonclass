db6c54c1b73c170d55d2696d56d84f4dadeabcf7
Robert Nester Odhiambo <odhiamborobert447@gmail.com>
2026-09-10 20:05:44 +0300

    clicking next button

--- webscrapping school/Sportepsa_SCraper_JAckpot/main.py
+++ webscrapping school/Sportepsa_SCraper_JAckpot/main.py
@@ -1,24 +1,56 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 options = Options()
 options.add_argument("--start-maximized")
 
-# Selenium automatically:
-# 1. Detects Chrome
-# 2. Finds the appropriate ChromeDriver
-# 3. Downloads it if necessary
-# 4. Starts Chrome
 driver = webdriver.Chrome(options=options)
 
 try:
     driver.get("https://www.ke.sportpesa.com/en/mega-jackpot-pro/results")
 
-    print("Browser opened successfully")
-    print("Title:", driver.title)
-    print("URL:", driver.current_url)
+    # Manually interact with the page
+    input("Interact with the website, then press ENTER here...")
 
-    input("Press Enter to close...")
+    # Wait for the iframe
+    iframe = WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located(
+            (By.ID, "multijackpot-iframe")
+        )
+    )
+
+    # Switch Selenium's context into the iframe
+    driver.switch_to.frame(iframe)
+
+    print("Switched into iframe.")
+
+    # Now search INSIDE the iframe
+    link = WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((
+            By.CSS_SELECTOR,
+            "div.simple-horizontal-carousel__container > a.simple-horizontal-carousel__btn"
+        ))
+    )
+
+    print("Found link:")
+    print(link.get_attribute("href"))
+
+    # Scroll it into view
+    driver.execute_script(
+        "arguments[0].scrollIntoView({block: 'center'});",
+        link
+    )
+
+    # Click it
+    link.click()
+
+    print("Link clicked.")
+    print("Current URL:", driver.current_url)
+
+    input("Press ENTER to close...")
 
 finally:
     driver.quit()
