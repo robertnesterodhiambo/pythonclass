66b3f88a2ed79f37deafd2c27a46c7e407240152
Robert Nester Odhiambo <odhiamborobert447@gmail.com>
2026-09-16 13:20:34 +0300

    sets creations

---
+++ ALX_DS/sets.py
@@ -0,0 +1,15 @@
+# Creating sets
+
+fruits = {"apple", "banana", "cherry"}
+
+# REpetetive vlaues in sets are automatically removed
+
+numbers = {1, 2, 3, 4, 4, 5, 5}
+
+# Empty set
+
+empty_set = set()  # Use set() to create an empty set, not {}
+
+print(fruits)  # Output: {'banana', 'cherry', 'apple'}
+print(numbers)  # Output: {1, 2, 3, 4, 5}
+print(empty_set)  # Output: set()
