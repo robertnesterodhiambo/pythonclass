ae20f6676db1ffa32c4e9585b23a87ee19f5463c
Robert Nester Odhiambo <odhiamborobert447@gmail.com>
2026-09-17 10:55:36 +0300

    Dictionery creation

---
+++ ALX_DS/dictionery.py
@@ -0,0 +1,5 @@
+# Dictioneries are used to store data values in key:value pairs.
+
+user = {"Name": "Robert", "Age": 30, "Occupation": "Engineer"}
+print(user["Name"])  # Output: Robert
+print(user)  # Output: {'Name': 'Robert', 'Age': 30, 'Occupation': 'Engineer'}
