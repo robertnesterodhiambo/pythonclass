4f3273eb26b00b0c7ed4bb7419ae021097edb7df
Robert Nester Odhiambo <odhiamborobert447@gmail.com>
2026-09-17 11:05:05 +0300

    dictioneries complete

--- ALX_DS/dictionery.py
+++ ALX_DS/dictionery.py
@@ -3,3 +3,24 @@
 user = {"Name": "Robert", "Age": 30, "Occupation": "Engineer"}
 print(user["Name"])  # Output: Robert
 print(user)  # Output: {'Name': 'Robert', 'Age': 30, 'Occupation': 'Engineer'}
+
+print(user.get("Age"))  # Output: 30
+
+# Adding a new key-value pair to the dictionary
+user["Age"] = 56
+user["Name"] = "John"
+user["Occupation"] = "Doctor"
+print(user)  # Output: {'Name': 'John', 'Age': 56, 'Occupation': 'Doctor'}
+
+# Updaing and Merging Dictioneries
+profile = {"Name": "Alice", "Age": 25,}
+profile.update({"Occupation": "Designer"})
+print(profile)  # Output: {'Name': 'Alice', 'Age': 25, 'Occupation': 'Designer'}
+
+d1 = {"A": 1, "B": 2}
+d2 = {"B": 99, "C": 3}
+
+merged_dict = {**d1, **d2}
+print(merged_dict)  # Output: {'A': 1, 'B': 99, 'C': 3}
+merged_dict = d1|d2
+print(merged_dict)  # Output: {'A': 1, 'B': 99, 'C': 3}
